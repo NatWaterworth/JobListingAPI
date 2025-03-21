955616ee9e68565242e5eb36d24dc19f0b345cb1
Nat Waterworth <nat.waterworthmindtech.global>
2025-03-21 16:21:06 +0000

    AppDbContext Setup
    Set up Entity Framework Core

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JobListingAPI.Models;
+
+namespace JobListingAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Job> Jobs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Job>()
+                .Property(j => j.Salary)
+                .HasPrecision(18, 2); // Ensures decimal(18,2) in SQL Server
+        }
+    }
+}
